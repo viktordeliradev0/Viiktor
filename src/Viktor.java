9d8527fa2cc2d23684b6b9f80a2bb5dc3343b86a
viktordeliradev0 <viktor 22>
2022-07-05 13:11:59 +0300

    MOney transfer

--- src/Viktor.java
+++ src/Viktor.java
@@ -1,2 +1,29 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Viktor {
+    public static void main(String[] args) {
+
+
+
+
+
+        Scanner input = new Scanner(System.in);
+        Integer BGN = input.nextInt();
+
+        System.out.println();
+        System.out.println("Tvoite pari v BGN : " + BGN);
+        System.out.println("Tvoite pari v USD " + BGN*1.64549 );
+
+
+
+    }
+
+
+
 }
+
+
+
+
+
